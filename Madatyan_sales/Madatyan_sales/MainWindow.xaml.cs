d5840a9bb915a2219b03030a0fe9cbc3dc496d0c
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-19 17:41:31 +0300

    Update MainWindow.xaml.cs

--- Madatyan_sales/Madatyan_sales/MainWindow.xaml.cs
+++ Madatyan_sales/Madatyan_sales/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace sales_Artak
+namespace sales_Madatyan
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
