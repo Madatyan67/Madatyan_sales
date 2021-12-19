773cb9ed86852bd2c90e21824611e09db73970ac
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-19 17:43:35 +0300

    Update App.xaml.cs

--- Madatyan_sales/Madatyan_sales/App.xaml.cs
+++ Madatyan_sales/Madatyan_sales/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace sales_Artak
+namespace sales_Madatyan
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
